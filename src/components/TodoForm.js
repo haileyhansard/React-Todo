dd11c8a00b450ba62d70e1716d116894b01a2cec
haileyhansard <haileyhansard@gmail.com>
2020-07-07 15:32:36 -0700

    Added notes taken with Vick 1 on 1 about passing down props example

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+//note from Vick: For this project only, I should move the handleChanges and handleSubmit to the App.js, also add this.props.handleSubmit and this.props.handleChanges on this page on lines 29 and 34. Also add on App.js line 88 handleChanges={this.handleChanges}. This is in order to pass down the handles as props, for this assignment. But in the real world, I would write it as I have done here.
 
 class TodoForm extends React.Component {
     constructor(props){
